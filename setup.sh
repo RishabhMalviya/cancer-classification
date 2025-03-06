e96146db8b274347f2eea8802f6deff59c21bf87
Ubuntu <ubuntu@ip-172-31-25-70.us-west-1.compute.internal>
2025-03-06 03:49:48 +0000

    cookiecutter skeleton init

---
+++ setup.sh
@@ -0,0 +1,12 @@
+pyenv install --skip-existing
+poetry install --with dev
+
+aws s3 mb s3://rishabhmalviya---cancer-classification --region us-west-1
+
+git init
+git add .
+git commit -m 'cookiecutter skeleton init'
+git branch -M main
+git remote add origin git@github.com:RishabhMalviya/cancer-classification.git
+git push --set-upstream origin main
+
