0abd3f87777c747770e98f321c24b2c1699c81d8
Rishabh Malviya <rishabh.malviya.america@gmail.com>
2025-03-14 04:54:21 +0000

    init git repo and s3 bucket only if not already done

--- setup.sh
+++ setup.sh
@@ -1,12 +1,21 @@
+# Initialize git repo
+if [ ! -d .git ]; then
+    git init
+    git add .
+    git commit -m 'cookiecutter skeleton init'
+    git branch -M main
+    git remote add origin git@github.com:RishabhMalviya/cancer-classification.git
+    git push --set-upstream origin main
+fi
+
+
+# Initialize S3 bucket for MLFlow
+if ! aws s3 ls "s3://rishabhmalviya---cancer-classification" 2>&1 | grep -q 'NoSuchBucket'; then
+    echo "Bucket already exists"
+else
+    aws s3 mb s3://rishabhmalviya---cancer-classification --region us-west-1
+fi
+
+# Initialize python virtual environment
 pyenv install --skip-existing
-export POETRY_VIRTUALENVS_IN_PROJECT=true && poetry install
-
-aws s3 mb s3://rishabhmalviya---cancer-classification --region us-west-1
-
-git init
-git add .
-git commit -m 'cookiecutter skeleton init'
-git branch -M main
-git remote add origin git@github.com:RishabhMalviya/cancer-classification.git
-git push --set-upstream origin main
-
+poetry install --all-groups
