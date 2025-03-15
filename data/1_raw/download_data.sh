fd6c1240672470a950595250a86802a34c00ccbd
Rishabh Malviya <rishabh.malviya.america@gmail.com>
2025-03-15 23:16:04 +0000

    add splits dir

--- data/1_raw/download_data.sh
+++ data/1_raw/download_data.sh
@@ -2,3 +2,4 @@ sudo apt-get update && sudo apt-get install -y unzip
 wget "https://zenodo.org/record/1214456/files/NCT-CRC-HE-100K.zip?download=1" -O NCT-CRC-HE-100K.zip
 unzip NCT-CRC-HE-100K.zip
 rm ./NCT-CRC-HE-100K.zip
+mkdir ./splits
